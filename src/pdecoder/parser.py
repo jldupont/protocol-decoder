0cb34da0623b86609fc4b4ccbc9401bed6f4c700
jldupont <jl@jldupont.com>
2011-11-30 21:51:17 -0500

    parser with list aggregation

--- src/pdecoder/parser.py
+++ src/pdecoder/parser.py
@@ -67,6 +67,7 @@
         >=
         <=
 '''
+import types
 from utils import xversa_split, xtotype, xop
 
 Op_Tokens=[
@@ -77,14 +78,27 @@ Group_Tokens=[
         "[", "]", "(", ")", ","
         ]
 
+Graph=[
+        ## object op pattern-binary
+        [(False, "id"), (False, "op"), (False, "binary")]
+        
+        ## object op pattern-hex
+       ,[(False, "id"), (False, "op"), (False, "hex")]
+       
+        ## object op [tuples]
+       ,[(False, "id"), (False, "op"), (True, "tuple")]
+       ]
+
 class ParserException(Exception):
     pass
 
-def parse(raw_text):
+def tokenize(raw_text):
     """
-    Parses the input text
+    Tokenize the input text, preserving the 'line number'
     
     @return list of tokens
+    
+    token:  (linenbr, (type, value))
     """
     def _xop(tokens):
         def _(x):
@@ -94,9 +108,71 @@ def parse(raw_text):
     raw_tokens=xversa_split(raw_text, tokens=Op_Tokens+Group_Tokens)
     tokens=map(xtotype, raw_tokens)    
     tokens=map(_xop(Op_Tokens+Group_Tokens), tokens)
-    
     return tokens
 
+def build_primitives(tokens):
+    """
+    Handles 'tuple' :  (x,y)
+    Handles 'list'  :  [ ... ]
+    
+    Token of the form:  (linenbr, (type, value))
+    """
+    def reducer(tk1, tk2):
+        #print "tk1:%s  tk2:%s" % (tk1, tk2)
+        #sys.stdout.flush()
         
+        l2, (_t2, v2) = tk2
+        if type(tk1)==types.TupleType:
+            liste=[tk1]
+        else:
+            liste=tk1
+            
+        #print "liste: %s" % liste
+            
+        ## inside the list
+        tail = liste.pop()
+        l1, (t1, v1) = tail
+        output=liste            
+       
+        ## case 1
+        if v1=="[" and v2=="]":
+            output.append((l1, ("list", [])))
+            return output
+        
+        ## case 2
+        if v1=="[":
+            output.append((l1, ("list", [v2])))
+            return output
+        
+        ## case 3
+        if v2=="]":
+            if t1=="list":
+                output.append((l1, ("list", v1)))
+            else:
+                output.append((l2, ("error", "] without [")))
+            return output
+        
+        ## case 4
+        if v2=="[":
+            output.append(tail)
+            output.append((l2, ("list", [])))
+            return output
+        
+        ## case 5: inside a list
+        if t1=="list":
+            v1.append(v2)
+            output.append((l1, ("list", v1)))
+            return output
+        
+        #print "<output: %s" % output    
+        output.append(tail)
+        output.append(tk2)
+        #print ">output: %s" % output
+        return output
+    
+    
+    r=reduce(reducer, tokens)
+    return r
+
     
     
--- src/pdecoder/tests.py
+++ src/pdecoder/tests.py
@@ -18,7 +18,7 @@ ipv4 = [
 def test_ipv4():
     """
     >>> import parser
-    >>> p=parser.parse(desc_ipv4)
+    >>> p=parser.tokenize(desc_ipv4)
     >>> print p
     [(1, ('id', 'ipv4')), (1, ('op', '=')), (1, ('op', '[')), (2, ('op', '(')), (2, ('int', '4')), (2, ('op', ',')), (2, ('id', 'version')), (2, ('op', ')')), (2, ('op', ',')), (2, ('op', '(')), (2, ('int', '4')), (2, ('op', ',')), (2, ('id', 'ihl')), (2, ('op', ')')), (2, ('op', ',')), (2, ('op', '(')), (2, ('int', '8')), (2, ('op', ',')), (2, ('id', 'tos')), (2, ('op', ')')), (2, ('op', ',')), (2, ('op', '(')), (2, ('int', '16')), (2, ('op', ',')), (2, ('id', '__len__')), (2, ('op', ')')), (2, ('op', ',')), (3, ('op', '(')), (3, ('int', '16')), (3, ('op', ',')), (3, ('id', 'id')), (3, ('op', ')')), (3, ('op', ',')), (3, ('op', '(')), (3, ('int', '3')), (3, ('op', ',')), (3, ('id', 'flags')), (3, ('op', ')')), (3, ('op', ',')), (3, ('op', '(')), (3, ('int', '13')), (3, ('op', ',')), (3, ('id', 'fragment_offset')), (3, ('op', ')')), (3, ('op', ',')), (4, ('op', '(')), (4, ('int', '8')), (4, ('op', ',')), (4, ('id', 'ttl')), (4, ('op', ')')), (4, ('op', ',')), (4, ('op', '(')), (4, ('int', '8')), (4, ('op', ',')), (4, ('id', '__next__')), (4, ('op', ')')), (4, ('op', ',')), (4, ('op', '(')), (4, ('int', '16')), (4, ('op', ',')), (4, ('id', 'checksum')), (4, ('op', ')')), (4, ('op', ',')), (5, ('op', '(')), (5, ('int', '32')), (5, ('op', ',')), (5, ('id', 'addr_src')), (5, ('op', ')')), (5, ('op', ',')), (5, ('op', '(')), (5, ('int', '32')), (5, ('op', ',')), (5, ('id', 'addr_dst')), (5, ('op', ')')), (5, ('op', ',')), (6, ('op', '(')), (6, (None, '*')), (6, ('op', ',')), (6, ('id', 'options')), (6, ('op', ')')), (6, ('op', ',')), (6, ('op', '(')), (6, (None, '*')), (6, ('op', ',')), (6, ('id', 'padding')), (6, ('op', ')')), (7, ('op', ']'))]
     """
@@ -30,6 +30,25 @@ def test_xversa_split():
     [(1, 'ipv4'), (1, '='), (1, '['), (2, '(4,'), (2, 'version),'), (2, '(4,'), (2, 'ihl),'), (2, '(8,'), (2, 'tos),'), (2, '(16,'), (2, '__len__),'), (3, '(16,'), (3, 'id),'), (3, '(3,'), (3, 'flags),'), (3, '(13,'), (3, 'fragment_offset),'), (4, '(8,'), (4, 'ttl),'), (4, '(8,'), (4, '__next__),'), (4, '(16,'), (4, 'checksum),'), (5, '(32,'), (5, 'addr_src),'), (5, '(32,'), (5, 'addr_dst),'), (6, '(*,'), (6, 'options),'), (6, '(*,'), (6, 'padding)'), (7, ']')]
     """
     
+def test_build_primitives():
+    """
+    >>> import parser
+    >>> tokens=[(0, ("op","[")),  (0, ("op","]"))]
+    >>> print parser.build_primitives(tokens)
+    [(0, ('list', []))]
+    >>> tokens=[(0, ("op","[")), (1, ("id","element")), (2, ("op","]"))]
+    >>> print parser.build_primitives(tokens)
+    [(0, ('list', ['element']))]
+    >>> tokens=[(0, ("op","[")), (1, ("id","element1")), (1, ("id","element2")), (2, ("op","]"))]
+    >>> print parser.build_primitives(tokens)
+    [(0, ('list', ['element1', 'element2']))]
+    >>> tokens=[(0, ("op","[")), (1, ("id","element1")), (1, ("id","element2")), (1, ("id","element3")), (2, ("op","]"))]
+    >>> print parser.build_primitives(tokens)
+    [(0, ('list', ['element1', 'element2', 'element3']))]
+    >>> tokens=[(0, ("id", "start1")), (0, ("id", "start2")), (0, ("op", "=")), (0, ("op","[")), (1, ("id","element1")), (1, ("id","element2")), (1, ("id","element3")), (2, ("op","]"))]
+    >>> print parser.build_primitives(tokens)
+    [(0, ('id', 'start1')), (0, ('id', 'start2')), (0, ('op', '=')), (0, ('list', ['element1', 'element2', 'element3']))]
+    """
     
 
 doctest.testmod()
